379ffc864aa6e87fae332a5839ff655f7426b717
MrKaStep <kalininstep@mail.ru>
2016-11-01 23:19:32 +0300

    InplaceMerge completed

--- timSort/timSort/inplaceMerge.h
+++ timSort/timSort/inplaceMerge.h
@@ -14,20 +14,39 @@ template<class FwdIt, class Compare>
 void merge(FwdIt first, FwdIt middle, FwdIt last,
            FwdIt buffer, Compare comp);
 
+template<class FwdIt>
+void swapSegments(FwdIt a, FwdIt b, ui32 length);
+
+template<class RandIt>
+void sortSegments(RandIt first, RandIt last, ui32 length);
+template<class RandIt, class Compare>
+void sortSegments(RandIt first, RandIt last, ui32 length,
+                  Compare comp);
+
+template<class RandIt>
+void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length);
+template<class RandIt, class Compare>
+void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length,
+                     Compare comp);
+
 template<class RandIt>
 void inplaceMerge(RandIt first, RandIt middle, RandIt last);
 template<class RandIt, class Compare>
 void inplaceMerge(RandIt first, RandIt middle, RandIt last,
                   Compare comp);
 
+/*
+ * Implementation
+ */
+
+
 template<class BidirIt>
 void selectionSort(BidirIt first, BidirIt last) {
     selectionSort(first, last,
-          std::less<std::iterator_traits<BidirIt>::value_type>());
+                  std::less<std::iterator_traits<BidirIt>::value_type>());
 }
 template<class BidirIt, class Compare>
 void selectionSort(BidirIt first, BidirIt last, Compare comp) {
-    typedef std::iterator_traits<BidirIt>::value_type valueType;
     for (; first != last; ++first) {
         BidirIt minimum = first;
         for (BidirIt it = first; it != last; ++it)
@@ -59,11 +78,49 @@ void merge(FwdIt first, FwdIt middle, FwdIt last,
 }
 
 template<class FwdIt>
-void swapSegments(FwdIt a, FwdIt b, ui32 size) {
-    for (ui32 i = 0; i < size; ++i) {
+void swapSegments(FwdIt a, FwdIt b, ui32 length) {
+    if (a == b)
+        return;
+    for (ui32 i = 0; i < length; ++i) {
         std::iter_swap(a++, b++);
     }
 }
+
+template<class RandIt>
+void sortSegments(RandIt first, RandIt last, ui32 length) {
+    sortSegments(first, middle, last,
+                 std::less<std::iterator_traits<FwdIt>::value_type>());
+}
+template<class RandIt, class Compare>
+void sortSegments(RandIt first, RandIt last, ui32 length, Compare comp) {
+    for (; first != last; first += length) {
+        RandIt minimum = first;
+        for (RandIt it = first; it != last; ++it)
+            if (*it != *minimum && comp(*it, *minimum) ||
+                comp(it[length - 1], minimum[length - 1]))
+                minimum = it;
+        swapSegments(minimum, first, length);
+    }
+}
+
+template<class RandIt>
+void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length) {
+    reorderSegments(first, middle, last, length,
+                    std::less<std::iterator_traits<RandIt>::value_type>());
+}
+template<class RandIt, class Compare>
+void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length,
+                     Compare comp) {
+    ui32 segmentsCount = (last - first) / length;
+    if (middle >= first + length * segmentsCount)
+        return;
+    RandIt middleSegmentBegin = first + length * ((middle - first) / length);
+    RandIt lastSegmentBegin = first + length * (segmentsCount - 1);
+    swapSegments(middleSegmentBegin, lastSegmentBegin, length);
+    sortSegments(first, lastSegmentBegin, length, comp);
+}
+
+
 
 template<class RandIt>
 void inplaceMerge(RandIt first, RandIt middle, RandIt last) {
@@ -72,14 +129,43 @@ void inplaceMerge(RandIt first, RandIt middle, RandIt last) {
 }
 template<class RandIt, class Compare>
 void inplaceMerge(RandIt first, RandIt middle, RandIt last, Compare comp) {
-    typedef std::iterator_traits<RandIt>::value_type valueType;
-    ui32 length = last - first;
-    ui32 segmentLength;
-    for (segmentLength = 0;(segmentLength + 1) * (segmentLength + 1) <= 
+    if (last - first < 17) {
+        selectionSort(first, last, comp);
+        return;
+    }
+    ui32 totalLength = last - first;
+    ui32 leftLength = middle - first;
+    ui32 rightLength = last - middle;
+    ui32 segmentLength = 0;
+    while ((segmentLength + 1) * (segmentLength + 1) <= totalLength)
+        ++segmentLength;
+    ui32 segmentsCount = totalLength / segmentLength;
 
+    if (middle < first + segmentLength * segmentsCount) {
+        reorderSegments(first, middle, last, segmentLength, comp);
+        RandIt lastSegmentBegin = first + segmentLength * (segmentsCount - 1);
+        for (ui32 i = 0; i + 2 < segmentsCount; ++i)
+            merge(first + segmentLength * i,
+                  first + segmentLength * (i + 1),
+                  first + segmentLength * (i + 2),
+                  lastSegmentBegin,
+                  comp);
 
-
-
-
+    }
+    ui32 badSegmentLength = segmentLength + totalLength % segmentLength;
+    RandIt badSegmentBegin = last - badSegmentLength;
+    selectionSort(last - 2 * badSegmentLength, last, comp);
+    RandIt toMerge = badSegmentBegin;
+    while (toMerge != first) {
+        RandIt mergeFirst = first + badSegmentLength <= toMerge ?
+            toMerge - badSegmentLength : first;
+        merge(mergeFirst, 
+              toMerge, 
+              toMerge + badSegmentLength, 
+              badSegmentBegin,
+              comp);
+        toMerge = mergeFirst;
+    }
+    selectionSort(badSegmentBegin, last);
     return;
 }
--- timSort/timSort/main.cpp
+++ timSort/timSort/main.cpp
@@ -3,8 +3,8 @@
 #include "inplaceMerge.h"
 
 int main() {
-    std::vector<int> a = { 9, 8, 4, 3, 1, 10, 7, 6, 5, 2 };
-    selectionSort(a.begin(), a.end(), [](int a, int b) {return a > b; });
+    std::vector<int> a = { 1, 5, 6, 8, 9, 2, 3, 4, 7, 10 };
+    inplaceMerge(a.begin(), a.begin() + 5, a.end());
     for (int i = 0; i < 10; ++i)
         printf("%d ", a[i]);
     system("pause");
