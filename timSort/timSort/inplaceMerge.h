ad4f8d880467b19f7ac37b8eccd7268aeb9f2a86
MrKaStep <kalininstep@mail.ru>
2016-11-02 03:22:37 +0300

    Yandex.Contest submit

--- timSort/timSort/inplaceMerge.h
+++ timSort/timSort/inplaceMerge.h
@@ -1,5 +1,7 @@
 #pragma once
-#include <algorithm>
+
+#include <functional>
+
 typedef unsigned int ui32;
 
 template<class BidirIt>
@@ -64,11 +66,11 @@ void selectionSort(BidirIt first, BidirIt last, Compare comp) {
 template<class BidirIt>
 void insertionSort(BidirIt first, BidirIt last) {
     insertionSort(first, last,
-                  std::less<std::iterator_traits<BidirIt>::value_type>());
+                  std::less<typename std::iterator_traits<BidirIt>::value_type>());
 }
 template<class BidirIt, class Compare>
 void insertionSort(BidirIt first, BidirIt last, Compare comp) {
-    typedef std::iterator_traits<BidirIt>::value_type valueType;
+    typedef typename std::iterator_traits<BidirIt>::value_type valueType;
     for (BidirIt toPush = std::next(first); toPush != last; ++toPush) {
         valueType val = *toPush;
         BidirIt cur = toPush;
@@ -84,7 +86,7 @@ template<class FwdIt>
 void merge(FwdIt first, FwdIt middle, FwdIt last,
            FwdIt buffer) {
     merge(first, middle, last,
-          std::less<std::iterator_traits<FwdIt>::value_type>());
+          std::less<typename std::iterator_traits<FwdIt>::value_type>());
 }
 template<class FwdIt, class Compare>
 void merge(FwdIt first, FwdIt middle, FwdIt last,
@@ -96,9 +98,13 @@ void merge(FwdIt first, FwdIt middle, FwdIt last,
     FwdIt right = middle;
     FwdIt write = first;
     FwdIt buffLast = buffer + (middle - first);
-    while (write != last && left != buffLast)
+    while (write != last && left != buffLast && right != last)
         std::iter_swap(write++,
                        comp(*left, *right) ? left++ : right++);
+    while (left != buffLast)
+        std::iter_swap(write++, left++);
+    while (right != last)
+        std::iter_swap(write++, right++);
 }
 
 template<class FwdIt>
@@ -112,15 +118,15 @@ void swapSegments(FwdIt a, FwdIt b, ui32 length) {
 
 template<class RandIt>
 void sortSegments(RandIt first, RandIt last, ui32 length) {
-    sortSegments(first, middle, last,
-                 std::less<std::iterator_traits<FwdIt>::value_type>());
+    sortSegments(first, last, length,
+                 std::less<typename std::iterator_traits<RandIt>::value_type>());
 }
 template<class RandIt, class Compare>
 void sortSegments(RandIt first, RandIt last, ui32 length, Compare comp) {
     for (; first != last; first += length) {
         RandIt minimum = first;
         for (RandIt it = first; it != last; it += length)
-            if (*it != *minimum && comp(*it, *minimum) ||
+            if (comp(*it, *minimum) || *it == *minimum &&
                 comp(it[length - 1], minimum[length - 1]))
                 minimum = it;
         swapSegments(minimum, first, length);
@@ -130,7 +136,7 @@ void sortSegments(RandIt first, RandIt last, ui32 length, Compare comp) {
 template<class RandIt>
 void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length) {
     reorderSegments(first, middle, last, length,
-                    std::less<std::iterator_traits<RandIt>::value_type>());
+                    std::less<typename std::iterator_traits<RandIt>::value_type>());
 }
 template<class RandIt, class Compare>
 void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length,
@@ -147,7 +153,7 @@ void reorderSegments(RandIt first, RandIt middle, RandIt last, ui32 length,
 template<class RandIt>
 void inplaceMerge(RandIt first, RandIt middle, RandIt last) {
     inplaceMerge(first, middle, last,
-                 std::less<std::iterator_traits<RandIt>::value_type>());
+                 std::less<typename std::iterator_traits<RandIt>::value_type>());
 }
 template<class RandIt, class Compare>
 void inplaceMerge(RandIt first, RandIt middle, RandIt last, Compare comp) {
